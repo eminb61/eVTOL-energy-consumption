625cb739f776f93b9eb54edf3991b30e01c1d33d
Raiyan Rizwan <raiyanrizwan@berkeley.edu>
2023-12-28 11:50:13 -0800

    new route with destination heading built in

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ import subprocess
 
 wind_speeds = list(range(0, 50, 5)) # 5, 10, ... 50 mph
 wind_directions = list(range(0, 365, 15)) # 0, 15, ... 180 degrees
-file_path = 'input_route_60_miles.csv'
+file_path = 'sfo_sjc_route_60_miles.csv'
 
 for ws in wind_speeds:
     for wd in wind_directions:
